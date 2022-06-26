2b08f1cb84dae276d91aee35fd2243acf3e43bd6
Michael Albonetti <malbo24@gmail.com>
2022-06-26 00:14:27 -0500

    add file picker package into app, implement and test usage on macOS and IOS

--- lib/app/screens/pdf_list.dart
+++ lib/app/screens/pdf_list.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:pdf_viewer/app/widgets/app_bar.dart';
+import 'package:file_picker/file_picker.dart';
 
 class PdfList extends StatefulWidget {
   const PdfList({Key? key}) : super(key: key);
@@ -9,6 +12,8 @@ class PdfList extends StatefulWidget {
 }
 
 class _PdfListState extends State<PdfList> {
+  File? currentFile;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -22,11 +27,26 @@ class _PdfListState extends State<PdfList> {
               const Text("PDF List Page"),
               TextButton(onPressed: (){
                 Navigator.pushNamed(context, "/viewer");
-              }, child: const Text("test next page"))
+              }, child: const Text("test next page")),
+              const ElevatedButton(onPressed: _getFile, child: Text("launch file picker")),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+
+
+Future<File?> _getFile() async{
+  FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+  if (result != null) {
+    File file = File(result.files.single.path!);
+    return file;
+  } else {
+    // User canceled the picker
+    return null;
   }
 }
