f29832c63c3a1cbf7f81fa3d3fa5a98ea06b5048
Michael Albonetti <malbo24@gmail.com>
2022-06-29 18:08:15 -0500

    create PDFSlide class, begin to list of requirements and data flow

--- lib/app/widgets/list_placeholder_page.dart
+++ lib/app/widgets/list_placeholder_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class ListPlaceholderPage extends StatelessWidget {
   final VoidCallback openFileBrowser;
-  const ListPlaceholderPage({Key? key, required this.openFileBrowser}) : super(key: key);
+
+  const ListPlaceholderPage({Key? key, required this.openFileBrowser})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +12,29 @@ class ListPlaceholderPage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("No PDF files yet!", style: Theme.of(context).textTheme.displaySmall),
-        const Divider(height: 20.0,),
-        Text("to view files, open the file browser below!", style: Theme.of(context).textTheme.subtitle1,),
-        const Divider(height: 20.0,),
-        IconButton(onPressed: openFileBrowser, icon: Icon(Icons.add_circle_outline, color: Colors.red[300], size: 50.0,)),
+        Text("No PDF files yet!",
+            style: Theme.of(context).textTheme.displaySmall),
+        const Divider(
+          height: 20.0,
+        ),
+        Text(
+          "to view files, open the file browser below!",
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        const Divider(
+          height: 20.0,
+        ),
+        IconButton(
+          onPressed: openFileBrowser,
+          icon: Icon(
+            Icons.add_circle_outline,
+            color: Colors.red[300],
+            size: 50.0,
+          ),
+        ),
       ],
     );
   }
 }
-
 
 //  TODO: define a text theme file, move all inline theming to file
---
+++ lib/app/widgets/pdf_slide.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// A PDF slide to preview either the first page of a document or an individual page for page scroll
+
+class PdfSlide extends StatefulWidget {
+  const PdfSlide({Key? key}) : super(key: key);
+
+  @override
+  State<PdfSlide> createState() => _PdfSlideState();
+}
+
+class _PdfSlideState extends State<PdfSlide> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//  TODO: slide should have a caption (file name or page number
+//  TODO: container should be a fixed size, no matter the size of the image/ page
+//  TODO: should have a placeholder text/ icon if the image/ page can not load
+//  TODO: should have an icon showing the number of pages in the document
+//  TODO: should have a loading icon while the data is loading
+//  TODO: optional swipe to remove
+
+//  TODO: possible 2 approaches:
+//    the data is loading in a service and a PdfSlide is generated each time new data is retrieved
+//    or - (the way I'm trying it first)
+// 1. SQL call to get list of files to be loaded
+// 2. an PdfSlide is created for each item retrieved
+// 3. if loading error (file moved or deleted) - file missing showed, option to remove
+// 4. if unable to generate first page - placeholder text
+// 5. after new PdfSlide created, add it to a list view
+// 6. swipe to clear file from cache
+// 7. possible "delete" function, remove from device
+// 8. clickable, to allow navigation to viewer
