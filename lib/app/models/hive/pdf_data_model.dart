dcd6a51f574525ee4f16d2d58ca82f682235d6a9
Michael Albonetti <malbo24@gmail.com>
2022-07-05 11:38:03 -0500

    create pdf data model, generate hive model

---
+++ lib/app/models/hive/pdf_data_model.dart
@@ -0,0 +1,21 @@
+import 'package:hive/hive.dart';
+
+part 'pdf_data_model.g.dart';
+
+@HiveType(typeId: 0)
+class PdfDataModel extends HiveObject {
+  @HiveField(0)
+  late String fileName;
+
+  @HiveField(1)
+  late String path;
+
+  @HiveField(2)
+  late DateTime addDate;
+
+  @HiveField(3)
+  late DateTime lastViewedDate;
+
+  @HiveField(4)
+  late bool pinned;
+}
---
+++ lib/app/models/hive/pdf_data_model.g.dart
@@ -0,0 +1,52 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pdf_data_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PdfDataModelAdapter extends TypeAdapter<PdfDataModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  PdfDataModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PdfDataModel()
+      ..fileName = fields[0] as String
+      ..path = fields[1] as String
+      ..addDate = fields[2] as DateTime
+      ..lastViewedDate = fields[3] as DateTime
+      ..pinned = fields[4] as bool;
+  }
+
+  @override
+  void write(BinaryWriter writer, PdfDataModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.fileName)
+      ..writeByte(1)
+      ..write(obj.path)
+      ..writeByte(2)
+      ..write(obj.addDate)
+      ..writeByte(3)
+      ..write(obj.lastViewedDate)
+      ..writeByte(4)
+      ..write(obj.pinned);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PdfDataModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
