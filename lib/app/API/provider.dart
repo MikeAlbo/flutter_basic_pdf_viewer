f68b910f22ebc7639d3410afe015e06b39fa53f0
Michael Albonetti <malbo24@gmail.com>
2022-07-05 11:36:57 -0500

    create pdf data model, generate hive model

---
+++ lib/app/API/provider.dart
@@ -0,0 +1,13 @@
+//  TODO: setup and init the db on load of the app
+//  TODO: create a model to store the pdf files
+//  TODO: hive methods to read and write from the object store
+//  TODO: interface with the app to get
+//  TODO: -- the current file being used
+//  TODO: -- the most recent viewed
+//  TODO: -- all files, sort functions
+//  TODO: -- favorite files
+//  TODO: put new file
+//  TODO: update file (saved)
+//  TODO: delete file (swiped to remove/ no longer available)
+
+// setup DB
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:pdf_viewer/app/app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+const pdfFilesBox = "pdfFiles";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox<String>(pdfFilesBox);
   runApp(const PDFViewerApp());
 }
