0238849271559b5e1c711d8c0639701190fbfac6
Michael Albonetti <malbo24@gmail.com>
2022-08-06 16:40:34 -0500

    remove old todos, print statements, and commented out methods. clean up file

--- lib/app/API/previous_viewed_box_provider.dart
+++ lib/app/API/previous_viewed_box_provider.dart
@@ -1,17 +1,3 @@
-//  TODO: load data from box
-//  TODO: check to see if paths are still valid
-//  TODO: sort items that are pinned in pinned list
-//  TODO: enum to describe sort options
-//  TODO: sort items by enum/ generate titles for sections
-//  TODO: clear all function, with confirmation alert
-//  TODO: stream to broadcast List to UI
-//  TODO: entire list should be built in API and broadcast as whole
-//  TODO: handle pinning of an item
-//  TODO: handle deleting of an item
-//  TODO: handle invalid item workflow
-
-// check for valid file path
-
 import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:pdf_viewer/app/API/list/list_item_widget.dart';
@@ -24,9 +10,6 @@ import '../models/hive/pdf_data_model.dart';
 class PreviousViewedBoxProvider extends ChangeNotifier {
   static const String previousViewBox = "PreviousViewBox";
 
-  // static Box<PdfDataModel> _previousViewBox() =>
-  //     Hive.box<PdfDataModel>(previousViewBox);
-
   List<PdfDataModel> _fileList = [];
 
   List<PdfDataModel> get getFiles => _fileList;
@@ -34,12 +17,6 @@ class PreviousViewedBoxProvider extends ChangeNotifier {
   List<ListItemWidget> _listWidgets = [];
 
   List<ListItemWidget> get getItems => _listWidgets;
-
-  //List<PdfDataModel> get getFiles => _fileList;
-
-  PreviousViewedBoxProvider() {
-    //getListOfFiles();
-  }
 
   void loadListItems() {
     Box<PdfDataModel> box = Hive.box<PdfDataModel>(previousViewBox);
@@ -51,13 +28,9 @@ class PreviousViewedBoxProvider extends ChangeNotifier {
   void getListOfFiles() {
     Box<PdfDataModel> box = Hive.box<PdfDataModel>(previousViewBox);
     _fileList = box.values.toList();
-    print("get list of files: ${_fileList.length}");
-    // notifyListeners();
-    //ox.close();
   }
 
   void addFilesToBox({required List<PDFFileModel> files}) async {
-    //await Hive.openBox<PdfDataModel>(previousViewBox);
     Box<PdfDataModel> box = Hive.box<PdfDataModel>(previousViewBox);
 
     for (var file in files) {
@@ -68,8 +41,6 @@ class PreviousViewedBoxProvider extends ChangeNotifier {
           pinned: false,
           addDate: DateTime.now(),
           lastViewedDate: DateTime.now());
-      //_previousViewBox().add(model);
-      print("add file: ${model.fileName}");
       box.add(model);
     }
     notifyListeners();
@@ -79,14 +50,12 @@ class PreviousViewedBoxProvider extends ChangeNotifier {
     // for dev purpose, used to clear out entire list without warning
     Box<PdfDataModel> box = Hive.box<PdfDataModel>(previousViewBox);
     box.clear();
-    print("clear called");
     notifyListeners();
   }
 
   void removeItemFromBox({required int itemKey}) {
     Box<PdfDataModel> box = Hive.box<PdfDataModel>(previousViewBox);
     box.delete(itemKey);
-    print("item $itemKey -- has been removed");
     notifyListeners();
   }
 
